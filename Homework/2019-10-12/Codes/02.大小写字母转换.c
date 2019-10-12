4106b2dd0a7070604ec29800d542fed52482c8a8
AimerNeige <1227427929@qq.com>
2019-10-12 19:27:35 +0800

    Homework

---
+++ Homework/2019-10-12/Codes/02.大小写字母转换.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char srt, ret;
+	int det = 'a' - 'A';
+	scanf("%c", &srt);
+	if ( srt <= 'z' && srt >= 'a') {
+		ret = srt - det;
+	}
+	else if ( srt <= 'Z' && srt >= 'A' ) {
+		ret = srt + det;
+	}
+	printf("%c\n", ret);
+	return 0;
+}

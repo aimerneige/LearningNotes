cb3d26d2a51ccaf5201bda6538d4edcfb4b26956
AimerNeige <1227427929@qq.com>
2019-10-13 22:14:09 +0800

    233

--- Homework/2019-10-12/Codes/03.二进制转换.c
+++ Homework/2019-10-12/Codes/03.二进制转换.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+#define SIZE 8
+
 int main(int argc, char const *argv[])
 {
     int num, i;
     scanf("%d", &num);
     unsigned mask = 1u<<(sizeof(unsigned) * 8 - 1);
     for (i = 0; mask ; mask >>= 1, i++) {
-        if (i > ((sizeof(unsigned) * 8 - 1) - 8))
+        if (i > ((sizeof(unsigned) * 8 - 1) - SIZE))
             printf("%d", (num & mask)?1:0);
     }
     printf("\n");
